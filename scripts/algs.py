3e2535b691743e1b6a51d118ddf6401d8f7ca82e
egilbertson-ucsf <erin.n.gilbertson@gmail.com>
2020-01-13 12:59:21 -0800

    off by 1, bubble

--- scripts/algs.py
+++ scripts/algs.py
@@ -32,7 +32,7 @@ def bubblesort(x):
     notes: use bubble sort algorithm -- adapted from pseudocode in Cormen textbook
     """
     for i in range(len(x)):
-        for j in range(len(x)-1, i+1, -1):
+        for j in range(len(x)-1, i, -1):
             if x[j] < x[j-1]:
                 x[j], x[j-1] = x[j-1], x[j]
     return x
