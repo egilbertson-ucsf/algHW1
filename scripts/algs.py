61d364f1b9f86534a9a177d114b18b3f44ad134f
egilbertson-ucsf <erin.n.gilbertson@gmail.com>
2020-01-13 13:08:18 -0800

    greater than or equal to

--- scripts/algs.py
+++ scripts/algs.py
@@ -17,7 +17,7 @@ def insertionsort(x):
     for j in range(1, len(x)):
         key = x[j]
         i = j-1
-        while i > 0 and x[i] > key:
+        while i >= 0 and x[i] > key:
             x[i+1] = x[i]
             i = i-1
         x[i+1] = key
